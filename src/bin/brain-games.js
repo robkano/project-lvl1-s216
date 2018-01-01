a6c67188c37f584aa4f1ce825f20d738a11a760e
Robin Koinov <robkano@gmail.com>
2018-01-01 12:43:47 +0200

    import/export resolved

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,5 +1,5 @@
 #!/usr/local/bin/node
-import {question} from '../index'
+import { question } from '../index'
 console.log('Welcome to the Brain Games!');
 
-console.log(question);
+console.log(question());
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
-import readlineSync from 'readline-sync';
+
+const readlineSync = require('readline-sync');
 
 const question = () => {
     const userName = readlineSync.question('May I have your name? ');
     return 'Hello ' + userName + '!';
 };
 
-console.log(question());
+export { question };
 
 
