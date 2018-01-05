bdf0d2d5a4822c944702239c477ae3bddc524bb2
Robin Koinov <robkano@gmail.com>
2018-01-05 16:31:56 +0200

    initial adding to code

---
+++ src/games/balance.js
@@ -0,0 +1,31 @@
+import { cons } from 'hexlet-pairs';
+import gameProsses from '../';
+import genarateNumber from '../moduls/generateNumber';
+
+const mainQuestion = () => {
+  const message = 'Balance the given number.\n';
+  return message;
+};
+
+const gameGenerator = () => {
+  const balance = (num) => {
+    const array = num.toString().split('').sort();
+    const number = array.map(Number);
+
+    while (number[array.length - 1] - number[0] > 1) {
+      number[0] += 1;
+      number[array.length - 1] -= 1;
+      number.sort();
+    }
+    return number.join('');
+  };
+
+  const num = genarateNumber(1, 500);
+  const result = balance(num);
+  const question = `${num}`;
+  const questionAndResult = cons(question, result);
+
+  return questionAndResult;
+};
+
+export default () => gameProsses(gameGenerator, mainQuestion);
