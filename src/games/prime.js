a45e371cb76722c03becfcdeb85b4e0b57e1f5ff
Robin Koinov <robkano@gmail.com>
2018-01-06 15:49:08 +0200

    adding prime game to code

---
+++ src/games/prime.js
@@ -0,0 +1,26 @@
+import { cons } from 'hexlet-pairs';
+import gameProsses from '../';
+import genarateNumber from '../moduls/generateNumber';
+
+const mainQuestion = 'Is this number prime?\n';
+
+const gameGenerator = () => {
+  const isPrime = (num) => {
+    if (num < 2) {
+      return 'no';
+    }
+    for (let div = 2; div < num; div += 1) {
+      if (num % div === 0) {
+        return 'no';
+      }
+    }
+    return 'yes';
+  };
+
+  const num = genarateNumber(1, 100);
+  const result = isPrime(num);
+  const questionAndResult = cons(num, result);
+  return questionAndResult;
+};
+
+export default () => gameProsses(gameGenerator, mainQuestion);
