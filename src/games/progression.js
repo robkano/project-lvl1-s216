ba2950ef7f401cfac20cb2456c790452f25640f8
Robin Koinov <robkano@gmail.com>
2018-01-06 12:26:04 +0200

    new added to brain games

---
+++ src/games/progression.js
@@ -0,0 +1,25 @@
+import { cons } from 'hexlet-pairs';
+import gameProsses from '../';
+import generateSequence from '../moduls/generateSequence';
+
+const mainQuestion = () => {
+  const message = 'What number is missing in this progression?.\n';
+  return message;
+};
+
+const gameGenerator = () => {
+  const hideNumber = (arr) => {
+    const str = arr.map(String);
+    const i = Math.floor(Math.random() * 10);
+    const result = str[i];
+    str.splice(i, 1, '..');
+    return cons(str.join(' '), result);
+  };
+
+  const sequnce = generateSequence(2);
+  const questionAndResult = hideNumber(sequnce);
+
+  return questionAndResult;
+};
+
+export default () => gameProsses(gameGenerator, mainQuestion);
